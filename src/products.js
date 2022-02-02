6e37d01c96d0c2417334a3cef8bcff6d91340715
Jamie Bunch <JamieBunch@ymail.com>
2022-02-02 12:42:56 -0500

    mysql-meets-eexpress-jnb

---
+++ src/products.js
@@ -0,0 +1,53 @@
+const mysql = require(`mysql2`);
+
+const connection = mysql.createConnection({
+    host: '34.67.146.233',
+    user: 'root',
+    password: 'BuildingSoFlo',
+    database: 'Commerce'
+});
+
+// const query =`SELECT * FROM Products`;
+
+// connection.query(query, (err, results, fields,) => {
+
+//     if(err){
+//         console.log(err)
+//     }
+
+//     console.log (results)
+    
+// });
+
+const getAllProducts = async () => {
+    const query = `SELECT * From Products`;
+
+    const [results, fields] = await connection.promise().query(query)
+
+    console.log(results);
+
+    return results;
+}
+
+const createProduct = async (product) => {
+
+    const insertQuery = `INSERT INTO Products (Description, SKU, UserID)
+    VALUES ('${product.Description}', '${product.SKU}', ${product.UserID})`
+ 
+    const [results, fields] = await connection.promise().query(insertQuery)
+
+    console.log(results);
+
+    return results;
+
+    //getAllProducts()
+    createProduct({
+        description: "Jamie new Product",
+        sku: "bunch1234",
+        userId: 1
+    });
+}
+
+getAllProducts();
+
+connection.end();
